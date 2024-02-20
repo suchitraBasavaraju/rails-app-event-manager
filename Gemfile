795519f4b99b413baebc541baf2c85c190294023
suchitra <suchitra9049@gmail.com>
2024-02-20 19:28:52 +0530

    Add. dotenv-rails gem for environment variable setting
    (cherry picked from commit 85c9efa0c0608e5378648881bd1793794a2ab36f)

--- Gemfile
+++ Gemfile
@@ -45,7 +45,9 @@ gem "bootsnap", require: false
 # gem "image_processing", "~> 1.2"
 
 group :development, :test do
-  gem 'rspec-rails', '~> 5.0'
+  gem 'dotenv-rails'
+  gem 'rspec-rails', '~> 6.1'
+  gem 'factory_bot_rails'
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[ mri windows ]
 end
--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,11 @@ require "rails/all"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+Bundler.require(*Rails.groups)
+if Rails.env.development? || Rails.env.test?
+  Dotenv::Railtie.load
+end
+
 module EventManager
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
