fe3f847102bcb35d6651a9fa943c3da0a5d7ff98
suchitra <suchitra9049@gmail.com>
2024-02-21 17:36:52 +0530

    Refactor. remove redundant url assignment

--- spec/requests/events_controller_spec.rb
+++ spec/requests/events_controller_spec.rb
@@ -3,13 +3,15 @@ require 'rails_helper'
 RSpec.describe "Events request", type: :request do
 
   let(:user) { create(:user) }
+  let(:track_event_url) { %r{/api/events/track} }
+  let(:email_target_url) { %r{/api/email/target} }
+
   before do
     sign_in user
   end
 
   describe '#create_event_a' do
     it 'creates Event A and tracks the event' do
-      track_event_url = /api\/events\/track/
       WebMock.stub_request(:post, track_event_url).to_return(status: 200, body: `{ "msg" : "Event A" }`, headers: {})
 
       post "/events/create_event_a"
@@ -18,7 +20,6 @@ RSpec.describe "Events request", type: :request do
     end
 
     it 'Event A creation failed' do
-      track_event_url = /api\/events\/track/
       WebMock.stub_request(:post, track_event_url).to_return(status: 400)
 
       post "/events/create_event_a"
@@ -29,8 +30,6 @@ RSpec.describe "Events request", type: :request do
 
   describe '#create_event_b and send email event' do
     it 'creates Event A and tracks the event' do
-      track_event_url = /api\/events\/track/
-      email_target_url = /api\/email\/target/
       WebMock.stub_request(:post, track_event_url).to_return(status: 200, body: `{ "msg" : "Event B" }`, headers: {})
       WebMock.stub_request(:post, email_target_url).to_return(status: 200, body: `{ "campaignId" : 0, "recipientEmail": "user@example.com" }`, headers: {})
 
@@ -40,8 +39,6 @@ RSpec.describe "Events request", type: :request do
     end
 
     it 'creates Event B failed' do
-      track_event_url = /api\/events\/track/
-      email_target_url = /api\/email\/target/
       WebMock.stub_request(:post, track_event_url).to_return(status: 400)
       WebMock.stub_request(:post, email_target_url).to_return(status: 200, body: `{ "campaignId" : 0, "recipientEmail": "user@example.com" }`, headers: {})
 
@@ -51,8 +48,6 @@ RSpec.describe "Events request", type: :request do
     end
 
     it 'Email target failed for event B' do
-      track_event_url = /api\/events\/track/
-      email_target_url = /api\/email\/target/
       WebMock.stub_request(:post, track_event_url).to_return(status: 200, body: `{ "msg" : "Event B" }`, headers: {})
       WebMock.stub_request(:post, email_target_url).to_return(status: 400)
 
--- spec/services/iterable_service_spec.rb
+++ spec/services/iterable_service_spec.rb
@@ -1,13 +1,13 @@
 require 'rails_helper'
 RSpec.describe 'IterableService' do
 
+  let(:track_event_url) { %r{/api/events/track} }
+  let(:email_target_url) { %r{/api/email/target} }
+
   describe "send email notification" do
-    before do
-      @send_email_url = /api\/email\/target/
-    end
 
     it "send email notification successfully" do
-      WebMock.stub_request(:post, @send_email_url).to_return(status: 200, body: `{ "campaignId" : 0, "recipientEmail": "Event A" }`, headers: {})
+      WebMock.stub_request(:post, email_target_url).to_return(status: 200, body: `{ "campaignId" : 0, "recipientEmail": "Event A" }`, headers: {})
 
       email = "user@example.com"
       response = IterableService.send_email_notification(email)
@@ -16,7 +16,7 @@ RSpec.describe 'IterableService' do
     end
 
     it "send notification failed with bad request" do
-      WebMock.stub_request(:post, @send_email_url).to_return(status: 400, body: `{ "error" : "Event B creation" }`, headers: {})
+      WebMock.stub_request(:post, email_target_url).to_return(status: 400, body: `{ "error" : "Event B creation" }`, headers: {})
 
       email = "user@example.com"
       expect {
@@ -24,7 +24,7 @@ RSpec.describe 'IterableService' do
     end
 
     it "send notification failed with access failure" do
-      WebMock.stub_request(:post, @send_email_url).to_return(status: 401, body: `{ "error" : "Forbidden" }`, headers: {})
+      WebMock.stub_request(:post, email_target_url).to_return(status: 401, body: `{ "error" : "Forbidden" }`, headers: {})
       email = "user@example.com"
 
       expect {
@@ -34,12 +34,8 @@ RSpec.describe 'IterableService' do
 
   describe "create event" do
 
-    before do
-      @url = /api\/events\/track/
-    end
-
     it "create Event successfully" do
-      WebMock.stub_request(:post, @url).to_return(status: 200, body: `{ "msg" : "Event A" }`, headers: {})
+      WebMock.stub_request(:post, track_event_url).to_return(status: 200, body: `{ "msg" : "Event A" }`, headers: {})
       email = "user@example.com"
 
       response = IterableService.track_event("Event A", email)
@@ -48,14 +44,15 @@ RSpec.describe 'IterableService' do
     end
 
     it "create Event failed for bad request" do
-      WebMock.stub_request(:post, @url).to_return(status: 400, body: `{ "msg" : "Event A" }`, headers: {})
+      WebMock.stub_request(:post, track_event_url).to_return(status: 400, body: `{ "msg" : "Event A" }`, headers: {})
       email = "user@example.com"
+
       expect {
         IterableService.track_event("Event A", email) }.to raise_error("Event Creation Failed,Error: 400")
     end
 
     it "create Event failed for access issue" do
-      WebMock.stub_request(:post, @url).to_return(status: 401, body: `{ "msg" : "Event A" }`, headers: {})
+      WebMock.stub_request(:post, track_event_url).to_return(status: 401, body: `{ "msg" : "Event A" }`, headers: {})
       email = "user@example.com"
 
       expect {
